b253e806d54ad6483939e788b4353eea24d1b5a8
Trevor Tanner <141767781+trevorstanner@users.noreply.github.com>
2024-12-19 11:31:05 -0800

    Update tamalax.js

--- tamalax.js
+++ tamalax.js
@@ -1,11 +1,21 @@
-Ecwid.OnAPILoaded.add(function() {
-    console.log("JS API is loaded");
-    console.log("Ecwid store ID: "+Ecwid.getOwnerId());
-    
-    Ecwid.OnPageLoaded.add(function(page) {
-        console.log("Page DOM is loaded");
-        console.log("Page type is: "+page.type)
-    });
-});
+Ecwid.OnAPILoaded.add(function () {
+  console.log("JS API is loaded");
+  console.log("Ecwid store ID: " + Ecwid.getOwnerId());
 
-// Test
+  Ecwid.OnPageLoaded.add(function (page) {
+    console.log("Page DOM is loaded");
+    console.log("Page type is: " + page.type);
+  });
+
+  Ecwid.OnPageLoaded.add(function (page) {
+    if (page.type == "PRODUCT") {
+      console.log(
+        `
+       Page loaded!
+       Ecwid store ID is: ${Ecwid.getOwnerId()}
+       Product ID is: ${page.productId}
+        `
+      );
+    }
+  });
+});
