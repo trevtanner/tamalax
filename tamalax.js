3f4e4185e8219dc9e1ce9d5c1c1700370c1e7c1c
Trevor Tanner <trevtanner@gmail.com>
2025-10-21 12:49:12 -0700

    Update tamalax.js

--- tamalax.js
+++ tamalax.js
@@ -487,16 +487,36 @@ Ecwid.OnAPILoaded.add(function () {
     if (page.type == "PRODUCT" && page.name.includes("STX")) {
       console.log("This is an STX Product");
       console.log("Removing price and ability to purchase online");
-      const priceSpan = document.querySelector(
-        ".details-product-price__value.ec-price-item"
-      );
 
-      function updateButtonState() {
-        newBuyButton.disabled = true;
-        newBuyButton.textContent = "Avaliable In Store Only";
+      const customContainer = document.createElement("div");
+      customContainer.className = "my-custom-field-container";
+
+      const newBuyButton = document.createElement("button");
+      newBuyButton.className = "new-buy-button disabled";
+      newBuyButton.id = "new-buy-btn";
+      newBuyButton.textContent = "Please make all selections"; // Initial message
+      newBuyButton.disabled = true; // Disabled by default
+      customContainer.appendChild(newBuyButton);
+
+      const productDetailsActionPanel = document.querySelector(
+        ".product-details__action-panel"
+      );
+      if (productDetailsActionPanel) {
+        productDetailsActionPanel.parentElement.insertBefore(
+          customContainer,
+          productDetailsActionPanel
+        );
+      } else {
+        console.error(
+          "Could not find the target element to inject the form fields."
+        );
       }
-      priceSpan.textContent = ``;
-      updateButtonState();
+
+      //Design Configs
+      window.ec = window.ec || Object();
+      window.ec.storefront = window.ec.storefront || Object();
+      window.ec.storefront.product_details_show_buy_button = false;
+      window.ec.storefront.product_details_show_product_price = false;
     }
   });
 });
